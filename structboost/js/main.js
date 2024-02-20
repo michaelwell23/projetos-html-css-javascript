4c4a7e2446fa40601f48a901ebf32f85fa235c6f
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-02-20 15:00:28 -0300

    Criando responsividade do footer

--- structboost/js/main.js
+++ structboost/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const btnMobile = document.querySelector('.btn-mobile');
   const mobileMenu = document.querySelector('.mobile-menu');
   const closeBtn = document.querySelector('.close-btn');
+  const menuLinks = document.querySelectorAll('.mobile-menu a'); // Seleciona todos os links dentro do menu
 
   btnMobile.addEventListener('click', function () {
     mobileMenu.style.display =
@@ -10,5 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   closeBtn.addEventListener('click', function () {
     mobileMenu.style.display = 'none';
+  });
+
+  // Adiciona evento de clique para cada link dentro do menu
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      mobileMenu.style.display = 'none'; // Oculta o menu ao clicar em um link
+    });
   });
 });
